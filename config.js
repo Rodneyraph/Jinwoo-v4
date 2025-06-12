4de4fb2f9f4495168cbc00efc9e076e7d3bf228f
Rodneyraph <raphaelrodney119@gmail.com>
2025-06-13 00:21:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const defaultOwner = '263780166288';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '263780166288' ; // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '254745314237' ; // put your number here
 
 const ownerlist = ownervb.split(';');
 
